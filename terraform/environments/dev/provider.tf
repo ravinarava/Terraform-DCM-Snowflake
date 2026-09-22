30ca107214fcb894bdd6f8452816c6a4b69835ac
Narava <raviprasad.narava@accenture.com>
2026-09-22 16:57:23 +1000

    Terraform plan

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -1,6 +1,6 @@
 provider "snowflake" {
   authenticator = "WORKLOAD_IDENTITY"
-
+  workload_identity_provider = "OIDC"
   organization_name = var.snowflake_organization
   account_name      = var.snowflake_account
 
