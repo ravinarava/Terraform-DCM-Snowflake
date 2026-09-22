c1fafbfc86e86e547b368bc9749ee2d5c624a677
Narava <raviprasad.narava@accenture.com>
2026-09-22 15:10:21 +1000

    Terraform plan

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,9 +1,9 @@
-locals {
-  environment = "dev"
-}
-
 module "warehouse" {
   source = "../../modules/warehouse"
+
+  providers = {
+    snowflake = snowflake
+  }
 
   warehouse_name = "TF_DEMO_WH"
   warehouse_size = "XSMALL"
